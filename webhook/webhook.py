c465226f2de575e5e7e7e656bb2a1bc78fd1e431
Shabbir Suterwala <shabbirsuterwala@gmail.com>
2023-02-08 13:36:26 -0800

    redis client / db implementation

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -13,7 +13,32 @@ class KafkaClient:
 
 
 class Redis:
-    pass
+
+    NAMESPACES = {}
+    def __init__(self, url, namespace):
+        self.namespace = namespace
+        self.NAMESPACES[namespace] = {}
+
+    def set(self, db, key, value) -> None:
+        namespace = self.NAMESPACES[self.namespace]
+        db_dict = namespace.get(db, None)
+        if db_dict is None:
+            db_dict = namespace[db] = {}
+
+        db_dict[key] = value
+
+    def get(self, db, key):
+        namespace = self.NAMESPACES[self.namespace]
+        value = namespace[db][key]
+        return value
+
+    def contains(self, db, key) -> bool:
+        namespace = self.NAMESPACES[self.namespace]
+        db_dict = namespace.get(db, None)
+        if db_dict is None:
+            return False
+        return key in db_dict
+
 
 
 class HTTPClient:
