89c63afd391b0b3c005323075a7eef946ad3aefe
Shabbir Suterwala <shabbirsuterwala@gmail.com>
2023-02-08 14:30:14 -0800

    kafka cleanup

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -12,8 +12,29 @@ class KafkaServer:
         self.topics = {}        # topic_name --> messages
         self.consumers = {}     # consumer_name --> topic, pointer
 
-    def commit(self, consumer):
-        self.ptr += 1
+    def create_topic(self, topic, partitions):
+
+        # ToDo: create seperate queues for partitions
+        self.topics[topic] = []
+
+    def get_message(self, consumer_name):
+        topic, ptr = self.consumers.get(consumer_name, (None, 0))
+        while ptr >= len(self.topics.get(topic, [])):
+            time.sleep(1)
+
+        msg = self.topics[self.topic][ptr]
+        return msg
+
+    def commit_message(self, consumer_name):
+        topic, ptr = self.consumers.get(consumer_name, (None, 0))
+        if topic is not None:
+            return
+
+        ptr += 1
+        self.consumers[consumer_name] = self.topic, ptr
+
+    def send_message(self, topic, msg):
+        self.topics[topic].append(msg)
 
 
 g_kafka_server = KafkaServer()
@@ -29,21 +50,11 @@ class KafkaConsumer:
         self.kafka_server = g_kafka_server
 
     def receive(self):
-        _, ptr = self.kafka_server.consumers.get(self.name, (None, 0))
-        while ptr >= len(self.kafka_server.topics.get(self.topic, [])):
-            time.sleep(1)
-
-        msg = self.kafka_server.topics[self.topic][ptr]
+        msg = self.kafka_server.get_message(self.name)
         return msg
 
     def commit(self):
-        topic, ptr = self.kafka_server.consumers.get(self.name, (None, 0))
-        if topic is not None:
-            return
-
-        ptr += 1
-        self.kafka_server.consumers[self.name] = self.topic, ptr
-
+        self.kafka_server.commit_message(self.name)
 
 class KafkaProducer:
     def __init__(self, server_url, topic):
@@ -51,7 +62,7 @@ class KafkaProducer:
         self.kafka_server = g_kafka_server
 
     def send(self, msg):
-        self.kafka_server.topics[self.topic].append(msg)
+        self.kafka_server.send_message(self.topic, msg)
 
 
 class Redis:
